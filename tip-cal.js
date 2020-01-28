e19af1b67777cad20c586f31dbd9c9adeb798077
mattj299 <mateonas586@gmail.com>
2020-01-27 18:51:47 -0800

    First time uploading files on github. I am just testing it by uploading a project I made called Tip Calculator.

---
+++ tip-cal.js
@@ -0,0 +1,37 @@
+var billTotal = document.getElementById("bill-amount");
+var service = document.getElementById("service-select");
+var numPeople = document.getElementById("num-people");
+var multiplePeople = document.getElementById("each-text");
+
+var calculateTip = () => {
+
+    var serviceVal = service.options[service.selectedIndex];
+
+    if (billTotal.value == "" || serviceVal.value == "" || numPeople.value == "")
+    {
+        alert("Please enter values");
+        return;
+    }   
+
+    if (numPeople.value <= 1)
+    {
+        numPeople.value = 1;
+        multiplePeople.style.display = "none";
+    }
+
+    else 
+    {
+        multiplePeople.style.display = "block";
+    }
+    var total = ((billTotal.value * serviceVal.value) / numPeople.value);
+    total = total.toFixed(2);
+
+
+    document.getElementById("total-tip").innerHTML = "<span> $ </span>" + total;
+    document.getElementById("tip-amount").style.display = "block";
+
+}
+
+
+
+
