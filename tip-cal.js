7fecf9e20f62c0b86f48249458b712be20fe786c
mattj299 <mateonas586@gmail.com>
2021-03-22 20:55:03 -0700

    Deleting console log from js file.

--- tip-cal.js
+++ tip-cal.js
@@ -4,35 +4,26 @@ var numPeople = document.getElementById("num-people");
 var multiplePeople = document.getElementById("each-text");
 
 var calculateTip = () => {
+  var serviceVal = service.options[service.selectedIndex];
 
-    var serviceVal = service.options[service.selectedIndex];
-    console.log(billTotal);
+  if (
+    billTotal.value == "" ||
+    serviceVal.value == "" ||
+    numPeople.value == ""
+  ) {
+    alert("Please enter values");
+    return;
+  }
 
-    if (billTotal.value == "" || serviceVal.value == "" || numPeople.value == "")
-    {
-        alert("Please enter values");
-        return;
-    }   
+  if (numPeople.value <= 1) {
+    numPeople.value = 1;
+    multiplePeople.style.display = "none";
+  } else {
+    multiplePeople.style.display = "block";
+  }
+  var total = (billTotal.value * serviceVal.value) / numPeople.value;
+  total = total.toFixed(2);
 
-    if (numPeople.value <= 1)
-    {
-        numPeople.value = 1;
-        multiplePeople.style.display = "none";
-    }
-
-    else 
-    {
-        multiplePeople.style.display = "block";
-    }
-    var total = ((billTotal.value * serviceVal.value) / numPeople.value);
-    total = total.toFixed(2);
-
-
-    document.getElementById("total-tip").innerHTML = "<span> $ </span>" + total;
-    document.getElementById("tip-amount").style.display = "block";
-
-}
-
-
-
-
+  document.getElementById("total-tip").innerHTML = "<span> $ </span>" + total;
+  document.getElementById("tip-amount").style.display = "block";
+};
